7d01c0e3c69ac3f5bf79bc2003805d65de0e4e80
Wesley Candido <wcandido@shopmonkey.io>
2026-03-16 21:21:05 -0300

    removing bar code file

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -23,9 +23,9 @@ export default function About() {
 
     gsap.to(imgRef.current, {
       clipPath: 'inset(0% 0% 0% 0%)',
-      duration: 1.2,
-      ease: 'power3.out',
-      delay: 0.2,
+      duration: 2.4,
+      ease: 'power2.inOut',
+      delay: 0.3,
     })
 
     gsap.from('.about-content > *', {
